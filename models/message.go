3b8873dbbb13042e144130e8bf3981bf24a5e33e
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-11-01 17:36:26 +0700

    added message model

--- models/message.go
+++ models/message.go
@@ -4,5 +4,7 @@ import "github.com/google/uuid"
 
 type Message struct {
 	ID uuid.UUID	`gorm:"primary_key;column:id"`
-	UserId uuid.UUID `gorm:"primary_key;column:id"`
+	UserId uuid.UUID `gorm:"column:user_id"`
+	Content string `gorm:"not null;column:content"`
+	User User `gorm:"foreignKey:user_id;references:id"`
 }
